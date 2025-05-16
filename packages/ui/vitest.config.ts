b843aba84b438a22a57489fbe548f71feb06762e
mz_eommj2 <mz_eommj2@woowafriends.com>
2025-05-16 13:15:40 +0900

    storybook base 수정

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -26,6 +26,7 @@ const dirname =
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
   plugins: [vue(), spaFallback()],
+  base: '/ux/storybook/',
   test: {
     workspace: [
       {
